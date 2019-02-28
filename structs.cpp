3392b36dd761d19b786eb4b5914668ca55a2723e
Nils Gustafsson <nilsgust@kth.se>
2019-02-28 19:24:33 +0100

    structs

--- structs.cpp
+++ structs.cpp
@@ -16,11 +16,12 @@ const ll big = 1000000007;
 
 struct photo{
     bool horisontal;
-    vector<string> tags;
+    vector<int> tags;
 };
 
 struct problem{
     int N;
+    int T;
     vector<photo> photos;
 };
 
@@ -29,6 +30,8 @@ problem read_problem(){
     int N;
     cin >> N;
     result.N = N;
+    map<string,int> M;
+    int sts = 0;
     for(int c1 = 0; c1 < N; c1++){
         char ch;
         cin >> ch;
@@ -39,30 +42,26 @@ problem read_problem(){
         for(int c2 = 0; c2 < a; c2++){
             string s;
             cin >> s;
-            ph.tags.push_back(s);
+            if(M.find(s) == M.end()){
+                M[s] = sts;
+                sts++;
+            }
+            ph.tags.push_back(M[s]);
         }
+        sort(all(ph.tags));
         result.photos.push_back(ph);
     }
+    result.T = sts;
     return result;
 }
 
-void print_solution(vector<int> &solution){
+void print_solution(vector<pair<int,int> > &solution){
     cout << sz(solution) << "\n";
     for(int c1 = 0; c1 < sz(solution); c1++){
-        cout << solution[c1] << "\n";
+        cout << solution[c1].first;
+        if(solution[c1].second != -1){
+            cout <<" "<< solution[c1].second;
+        }
+        cout << "\n";
     }
-}
-
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    problem p = read_problem();
-    vi s;
-    for(int c1 = 0; c1 < p.N; c1++){
-        s.push_back(c1);
-    }
-    print_solution(s);
-
-    return 0;
 }
