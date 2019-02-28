8553db81b6765dd60d50d9262866ce677a17097a
Pepe <pe@pe.pe>
2019-02-28 19:42:30 +0100

    take best random

--- rand_dumb.py
+++ rand_dumb.py
@@ -1,32 +1,54 @@
 from random import *
 from itertools import *
+from time import *
 
-def score(
+def score(slides, tags):
+    total = 0
+    for slide in xrange(1,len(slides)):
+        left = set()
+        for pic in slides[slide-1]:
+            left = left | tags[pic]
+        right = set()
+        for pic in slides[slide]:
+            right = right | tags[pic]
+        inter = left & right
+        total += min(len(left) - len(inter), len(inter), len(right) - len(inter))
+    return total
 
 def pairwise(t):
     it = iter(t)
     return izip(it,it)
 
 n = int(raw_input())
-tags = []
+tags = [0]*n
 verts = []
 horis = []
-for photo in xrange(n):
+for pic in xrange(n):
     line = raw_input().split()
     orient = line[0]
-    tags = line[2:]
+    tags[pic] = set(line[2:])
     if orient == 'H':
-        horis.append([photo])
+        horis.append([pic])
     else:
-        verts.append(photo)
+        verts.append(pic)
 
-# Pair verts
-shuffle(verts)
-paired = zip(verts[::2], verts[1::2])
-if paired[-1][1] == None:
-    paired = paired[:-1]
-slides = horis + paired
-shuffle(slides)
+t0 = time()
+best_score = 0
+best = []
+
+while time()-t0 < 1:
+    # Pair verts
+    shuffle(verts)
+    paired = zip(verts[::2], verts[1::2])
+    if len(paired) > 0 and paired[-1][1] == None:
+        paired = paired[:-1]
+    slides = horis + paired
+    shuffle(slides)
+
+    new_score = score(slides, tags)
+    if new_score > best_score:
+        best_score = new_score
+        best = slides[:]
 
 print len(slides)
 for slide in slides:
